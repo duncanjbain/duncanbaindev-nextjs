f0b5fb352f84eba1e1d36f2fd8e0f9d26fe5ec9a
Duncan Bain <duncan@bain.io>
2021-07-15 21:52:06 +0100

    add uses page info

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+   "react/react-in-jsx-scope": "off",
+   "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+   "react/prop-types": "off"
+  },
+  extends: ["next", "next/core-web-vitals", 'plugin:react/recommended', 'airbnb', 'prettier'],
+  plugins: ['react', 'prettier']
+};
